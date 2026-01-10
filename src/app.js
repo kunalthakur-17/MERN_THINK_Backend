a2ca2ff0c49d9371d42c48a5d72dc47ebfc6a675
Kunal Thakur <141919988+kunalthakur-17@users.noreply.github.com>
2026-01-11 00:52:01 +0530

    deploy

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5174","http://localhost:5173", /^https:\/\/mern-think-frontend.*\.vercel\.app$/],
+    origin: ["http://localhost:5174","http://localhost:5173", /^https:\/\/mern-think-frontend.*\.vercel\.app$/, "https://www.kunalthakur.store"],
     credentials: true
 }));
 app.use(cookieParser());
